f0702863d7f8a5263b4d5df8cd3f22b64ae5eb47
Mayank <mayank2808sharma@gmail.com>
2026-07-21 19:56:41 +0530

    rg added

--- main.tf
+++ main.tf
@@ -1,22 +1,18 @@
 terraform {
   required_providers {
-    azurerm = {
-     source = "hashicorp/azurerm"
-     version = "<~4.80.0"
+    azurerm ={
+        version = "=4.80.0"
+        source = "hashicorp/azurerm"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider azurerm {
+features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "rg_metlife"
+resource "azurerm_resource_group" "rg1" {
+  name ="rg_pipeline"
   location = "east us"
-}
 
-resource "azurerm_resource_group" "rg" {
-  name = "rg_metlife"
-  location = "east us"
 }

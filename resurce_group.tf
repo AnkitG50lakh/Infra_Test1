5600d92f496803ebe7f6c5471cc1bf810e88c612
Ankit Goud <ankitgoud97@gmail.com>
2026-08-06 19:34:58 +0530

    file name change

---
+++ resurce_group.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm ={
+        version = "=4.80.0"
+        source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider azurerm {
+features {}
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name ="rg_pipeline1"
+  location = "east us"
+
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name ="rg_pipeline2"
+  location = "east us"
+
+}
